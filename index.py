a25235c6948e0ecf7dc4c702a58db85e4495656f
Francisco Hernandez Soto <centenaria@gmail.com>
2026-02-12 10:13:13 -0400

    multiplicacion

--- index.py
+++ index.py
@@ -3,8 +3,14 @@ lista = [1 , 2 , 3 , 4 ,5, 9 , 89]
 print(f'la longitud de la lista es : {len(lista)}')
 
 suma = 0
+multiplicacion = 1
 
 for i in lista :
     suma += i
 
 print(f'\nla suma de los valores de la lista es : {suma}')
+
+for i in lista :
+    multiplicacion *= i
+
+print(f'multiplicacion elementos lista : {multiplicacion}')
